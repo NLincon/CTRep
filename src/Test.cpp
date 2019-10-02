a349f59c5ed5e20ffde4b2b573ccc39105f4fef6
Никита <@DESKTOP-BOTE313>
2019-10-02 17:41:18 +0300

    Hal added

--- src/Test.cpp
+++ src/Test.cpp
@@ -2,15 +2,34 @@
 #include "ide_listener.h"
 #include "xml_listener.h"
 #include "cute_runner.h"
+#include "Hal.h"
 
 void thisIsATest() {
 	ASSERTM("start writing tests", false);	
+}
+
+void halSetupTest()
+{
+	int expectedResult = 0;
+	int actualResult = 0;
+
+	ASSERT_EQUAL(expectedResult, actualResult);
+}
+
+void halLoopTest()
+{
+	int expectedResult = 0;
+	int actualResult = 0;
+
+	ASSERT_EQUAL(expectedResult, actualResult);
 }
 
 bool runAllTests(int argc, char const *argv[]) {
 	cute::suite s { };
 	//TODO add your test here
 	s.push_back(CUTE(thisIsATest));
+	s.push_back(CUTE(halSetupTest));
+	s.push_back(CUTE(halLoopTest));
 	cute::xml_file_opener xmlfile(argc, argv);
 	cute::xml_listener<cute::ide_listener<>> lis(xmlfile.out);
 	auto runner = cute::makeRunner(lis, argc, argv);
